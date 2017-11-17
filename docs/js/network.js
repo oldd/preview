2e0f69e42d6c2f205501303744b529ea8dc042a4
colevandersWands <evan.t.cole@gmail.com>
2017-11-17 16:35:26 +0100

    adding md extension

--- docs/js/network.js
+++ docs/js/network.js
@@ -5,25 +5,34 @@
     // create an array with nodes
     var nodes = new vis.DataSet([
         {id: 00,    label: 'developering', 
-                    title: 'devving is collaboration and communication',
-                    url: 'https://github.com/jankeLearning/nodes/blob/master/sepco-projects-only/00-communications.md'}
+                    title: 'developering is collaboration and communication',
+                    url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/developering'},
+        {id: 01,    label: 'friendly code', 
+                    title: 'code is meant to be read, not written',
+                    url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/friendly-code'},
+        {id: 02,    label: 'apps', 
+                    title: 'app = {data, logic} + User Interaction',
+                    url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/apps'},                    
+        {id: 03,    label: 'git and Hub', 
+                    title: 'tools for sharing, collaborating, and preserving',
+                    url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/apps'},                                        
     ]);
 
     // create an array with edges
     var edges = new vis.DataSet([
-        {from: 00, to: 01, arrows: 'to, middle'},
-        {from: 01, to: 02, arrows: 'to, middle'},
-        {from: 01, to: 03, arrows: 'to, middle'},
-        {from: 02, to: 04, arrows: 'to, middle'},
-        {from: 03, to: 05, arrows: 'to, middle'},
-        {from: 04, to: 05, arrows: 'to, middle'},
-        {from: 04, to: 06, arrows: 'to, middle'},
-        {from: 05, to: 07, arrows: 'to, middle'},
-        {from: 06, to: 07, arrows: 'to, middle'},
-        {from: 05, to: 11, arrows: 'to, middle'},
-        {from: 07, to: 08, arrows: 'to, middle'},
-        {from: 07, to: 09, arrows: 'to, middle'},
-        {from: 07, to: 10, arrows: 'to, middle' }
+        {from: 00, to: 01, arrows: 'to'},
+        {from: 01, to: 02, arrows: 'to'},
+        {from: 01, to: 03, arrows: 'to'},
+        {from: 02, to: 04, arrows: 'to'},
+        {from: 03, to: 05, arrows: 'to'},
+        {from: 04, to: 05, arrows: 'to'},
+        {from: 04, to: 06, arrows: 'to'},
+        {from: 05, to: 07, arrows: 'to'},
+        {from: 06, to: 07, arrows: 'to'},
+        {from: 05, to: 11, arrows: 'to'},
+        {from: 07, to: 08, arrows: 'to'},
+        {from: 07, to: 09, arrows: 'to'},
+        {from: 07, to: 10, arrows: 'to' }
     ]);
 
     // options
