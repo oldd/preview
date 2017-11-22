b880f23d4b675d62e9e883138843750c126d5e7b
colevandersWands <evan.t.cole@gmail.com>
2017-11-22 19:08:09 +0100

    building

--- book_source/js/network.js
+++ book_source/js/network.js
@@ -8,59 +8,59 @@ var nodes = new vis.DataSet([
     {id: 01,    label: 'friendly code', title: 'code is meant to be read, not written \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/friendly-code.md',
-                level: 1, x: -400, y: -100},
+                level: 1, x: -450, y: -50},
     {id: 02,    label: 'apps', title: 'app = {data, logic} + User Interaction',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/apps.md',
-                level: 1, x: -400, y: 0},
+                level: 1, x: -400, y: 60  },
     {id: 03,    label: 'git and Hub', title: 'tools for sharing, collaborating, and preserving \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/git-and-hub.md',
-                level: 1, x: -350, y: 200 },
+                level: 1, x: -350, y: 175  },
     {id: 04,    label: 'planning apps', title: 'programmers code apps, developers plan them \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/understanding-problems.md',
-                level: 1, x: -350, y: 100 },
+                level: 1, x: -250, y: 100 },
     {id: 05,    label: 'pure functions', title: 'kind of like a factory \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/pure-functions.md',
-                level: 1, x: -300, y: -150 },
+                level: 1, x: -300, y: -100 },
     {id: 06,    label: 'OOP in JS', title: 'single-purpose bags \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/oop-in-js.md',
-                level: 1, x: -300, y: -50 },
+                level: 1, x: -300, y: 0 },
     {id: 07,    label: 'specs', title: 'how developers talk about code \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/specs.md',
-                level: 1, x: -200, y: -100 },
+                level: 1, x: -200, y: -50 },
     {id: 09,    label: 'testing 101', title: 'just the basics, all the basics \n -- in progress --',
                 group: 'md',
                 url: 'https://github.com/jankeLearning/sample-lesson/blob/master/nodes/markdown/testing-101.md',
-                level: 1, x: -100, y: -150 },
+                level: 1, x: -50, y: -100 },
     {id: 11,    label: 'objects as apps', title: 'thinking of objects as self-contained units of user story \n -- in progress --',
                 group: 'we',
                 url: 'https://github.com/jankeLearning/sample-lesson/tree/master/nodes/worked-example/objects-as-apps',
-                level: 2, x: -100, y: -50 },
+                level: 2, x: 0, y: 0 },
     {id: 12,    label: 'model objects', title: 'not glamorous, but worth your time. we promise \n -- in progress --',
                 group: 'we',
                 url: 'https://github.com/jankeLearning/sample-lesson/tree/master/nodes/worked-example/model-objects',
-                level: 2, x: 130, y: -50 },
+                level: 2, x: 150, y: -50 },
     {id: 13,    label: 'logic vs framework', title: 'probably the most important lesson here \n -- in progress --',
                 group: 'we',
                 url: 'https://github.com/jankeLearning/sample-lesson/tree/master/nodes/worked-example/logic-vs-framework',
-                level: 2, x: 40, y: 25 },
+                level: 2, x: 300, y: 25 },
     {id: 14,    label: 'repoing', title: 'how to build useful github repos \n -- in progress --',
                 group: 'we',
                 url: 'https://github.com/jankeLearning/sample-lesson/tree/master/nodes/worked-example/repoing',
-                level: 2, x: 130, y: 100 },
+                level: 2, x: 200, y: 150 },
     {id: 15,    label: 'project planning', title: 'all your code is no good if you never write it \n -- in progress --',
                 group: 'we',
                 url: 'https://github.com/jankeLearning/sample-lesson/tree/master/nodes/worked-example/project-planning',
-                level: 2, x: -100, y: 100 },
+                level: 2, x: -100, y: 75 },
     {id: 16,    label: 'final project', title: 'put it all together in this guided project \n -- in progress --',
                 group: 'project',
                 url: 'https://github.com/jankeLearning/sample-lesson/tree/master/nodes/projects/janke-project \n -- in progress --',
-                level: 3, x: 250, y: 50 }
+                level: 3, x: 450, y: 50 }
 ]);
 
 var edges = new vis.DataSet([
@@ -173,6 +173,9 @@ var options = {
           }
         } 
       }
+    },
+    nodes: {
+      shape: 'circle'
     }
 };
 
@@ -182,13 +185,29 @@ var data = {
     nodes: nodes,
     edges: edges
 };
+
 var network = new vis.Network(container, data, options);
 
-// event listener
+// event listeners
 network.on("selectNode", function (params) {
     if (params.nodes.length === 1) {
         var node = nodes.get(params.nodes[0]);
         window.open(node.url, '_blank');
+        window.open('#');
     }
 });
+
+network.on("selectNode", function (params) {
+  var node = nodes.get(params.nodes[0]);
+  node.color = '#ffff00';
+  nodes.update(node);
+  console.log(node);
+});
+
+network.on("deselectNode", function (params) {
+  var node = nodes.get(params.previousSelection.nodes[0]);
+  node.color = options.groups[node.group].color.background;
+  nodes.update(node);
+  console.log(node);
+}); 
  
